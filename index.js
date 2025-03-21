1a234f98bbf68646f425cdc763f7301186f60efe
sumedhvijaypagare <sumedhpagare13@gmail.com>
2025-03-21 14:47:27 +0530

    updated backend with frontend integration

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 require("dotenv").config()
+const path = require("path")
 
 const app = express()
 app.use(express.json())
+app.use(express.static("dist"))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://samplee-34ag.onrender.com",
     credentials: true
 }))
 
 app.use("/api/todo", require("./routes/todo.routes"))
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "resource not found" })
+    // res.status(404).json({ message: "resource not found" })
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 app.use((err, req, res, next) => {
     console.log(err)
